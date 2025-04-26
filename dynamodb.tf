167bb1db1b48977f40f1df8cca57a5be84f9463b
ppabis <piotr@pabis.eu>
2025-04-26 11:43:12 +0200

    Create table for temporary title storage

---
+++ dynamodb.tf
@@ -0,0 +1,30 @@
+variable "table_name" {
+  type        = string
+  description = "Name of the DynamoDB table"
+}
+
+resource "aws_dynamodb_table" "items" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "uid"
+
+  attribute {
+    name = "uid"
+    type = "S"
+  }
+
+  attribute {
+    name = "expireAt"
+    type = "N"
+  }
+
+  attribute {
+    name = "title"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "expireAt"
+    enabled        = true
+  }
+}
