1bbe55f9de1a23c4d3e1ceeb689ce3fb831d2956
ppabis <piotr@pabis.eu>
2025-04-28 06:07:11 +0200

    Create list lambda with API GW

---
+++ apigw.tf
@@ -0,0 +1,42 @@
+variable "api_name" {
+  type        = string
+  description = "Name of the API Gateway"
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name = var.api_name
+}
+
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "get-list" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
+  http_method = aws_api_gateway_method.get.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda["list"].invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "prod" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name  = "prod"
+
+  depends_on = [
+    aws_api_gateway_integration.get-list
+  ]
+
+  variables = {
+    "deployed_version" = "1" # Change this to force deployment, otherwise you have to do it manually
+  }
+}
+
+output "api-gateway" {
+  value = aws_api_gateway_deployment.prod.invoke_url
+}
---
+++ lambda.tf
@@ -0,0 +1,38 @@
+data "archive_file" "zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambdas"
+  output_path = "${path.module}/lambdas.zip"
+}
+
+resource "aws_lambda_function" "lambda" {
+  for_each = toset(["list", "tag", "upload"])
+
+  function_name    = "photos-lambda-${each.key}"
+  role             = aws_iam_role.LambdaPhotosRole.arn
+  handler          = "${each.key}.lambda_handler"
+  runtime          = "python3.12"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.images.bucket
+    }
+  }
+}
+
+resource "aws_lambda_function_url" "lambda_url" {
+  function_name      = aws_lambda_function.lambda["list"].function_name
+  authorization_type = "NONE"
+}
+
+resource "aws_lambda_permission" "api" {
+  for_each      = toset(["list", "upload"])
+  function_name = aws_lambda_function.lambda[each.key].function_name
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  principal     = "apigateway.amazonaws.com"
+  action        = "lambda:InvokeFunction"
+}
+
+output "lambda_url" {
+  value = aws_lambda_function_url.lambda_url
+}
